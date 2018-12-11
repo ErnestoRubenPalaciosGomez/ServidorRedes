71605efc692736b8f2eb318c522a3f0528e71a73
Ernesto Palacios <ernesto_palacios@ciencias.unam.mx>
2018-12-11 14:35:44 -0600

    lo que llevo

---
+++ clienteRedes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import socket # importamos los modulos para trabajar con sockets
+import sys #importamos los modulos para poder acceder a la linea de comandos 
+
+def inicio(ip_servidor,puerto):
+
+	#Creamos un objeto de tipo socket para el servidor
+	s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+	try:
+		#Nos conectamos al al servidor con el metodo connect. Tiene dos parametros
+		#El primero es la IP del servidor y el segundo es el puerto de conexion 
+		s.connect((ip_servidor , int(puerto)))
+	except:
+		print "Protocol Error 41 \n41 No se pudo conectar con el servidor"
+	else:
+		#Creamos un bucle para retener la conexion
+		while True:
+			#mensaje para poder establecer la conexion
+			mensaje = bytearray([10])
+			#Con la distancia del objeto servidor (s) y el metodo , send enviamos el mensaje
+			s.send(mensaje)
+			respuesta = s.recv(1024)
+			print respuesta
+			mensaje = raw_input("Mensaje a enviar : ")
+			"""
+			#Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
+			mensaje = raw_input("Mensaje a enviar : ")
+			#Con la distancia del objeto servidor (s) y el metodo , send enviamos el mensaje
+			s.send(mensaje)
+			#Obtenemos la contestacion del cliente con
+			"""
+			respuesta = s.recv(1024)
+			#respuesta = s.recv(1024)
+			print respuesta
+			#Si por alguna razon el mensaje es close se cierra la conexion
+			if mensaje == "close":
+				break
+		#Imprimimos adios cuando se cierre la conexion
+		print "Adios"
+		#Cerramos la instancia de servidor
+		s.close()
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		_ , ip_servidor , puerto = sys.argv
+		inicio (ip_servidor , puerto)	
+	else:
+		print "Error en los parametros"
---
+++ servidorRedes.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+import socket #importamos el modulo de socket
+from threading import Thread #importamos para los threads
+import random
+
+
+HOST = "" #el host de donde vamos a poder recibir peticiones
+PUERTO = 9999  #el puerto donde estaremos escuchando
+
+POKEMONES_DISPONIBLES = ["Gengar" , "Yveltal" , "Blaziken" , "Alakazam" , "Bisharp" , "Charizard"]
+
+#clase para cada uno de los hilos del servidor
+class ServidorHilo(Thread):
+	def __init__(self , socket_cliente , id):
+		Thread.__init__(self) #inicializamos el padre
+
+		self.socket_cliente = socket_cliente
+		self.id = id
+		self.mandar_pokemon = bytearray([10])
+	def run (self):
+		while True:
+			#Recibir datos del cliente
+			datos = self.socket_cliente.recv(1024)
+			datos = list(datos)
+			if datos[0] == self.mandar_pokemon:
+				self.socket_cliente.send("¿Te gustaria capturar a el pokemon " + )
+
+			#Si el mensaje recibido es la palabra close se cierra la aplicacion
+			if datos == "close":
+				break
+			#si se reciben los datos nos muestra la IP y el mensaje recibido
+			print  "el cliente "+ str(self.id) + " dice: " , datos
+			#Devolvemos el mensaje al cliente
+			
+
+#Funcion para iniciar las conexiones
+def inicio():
+	#instanciamos un objeto para trabajar con el socket
+	socket_servidor = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+	#Con el metodo bind indicamos que puerto debe de escuchar 
+	socket_servidor.bind((HOST , PUERTO))
+
+	#Aceptamos conexciones entrantes con el metodo listen , y ademas aplicamos como parametro 
+	#el numero de conexiones que podemos aceptar
+	socket_servidor.listen(0)
+
+	salir = False
+
+	print ("esperando cliente ...")
+	id = 1
+	while not salir:
+		#Instanciamos un objeto sc (socket cliente) para recibir datos , al recibir datos este 
+		#devolvera tambien un obejto que representa la tupla:IP , puerto
+		socket_cliente , addr = socket_servidor.accept()
+		
+		hilo = ServidorHilo(socket_cliente , id) #creamos un hilo para atender a ese cliente
+		hilo.start() # mandamos a correr el hilo 
+		print ("un cliente se ha conectado el cliente " + str(id))
+		id += 1
+
+if __name__ == '__main__':
+	inicio()
+
+
+
+
+
+
+
+
+
+
+
+"""
+#Funcion para iniciar las conexiones
+def inicio():
+	#instanciamos un objeto para trabajar con el socket
+	socket_servidor = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+	#Con el metodo bind indicamos que puerto debe de escuchar 
+	socket_servidor.bind(("" , 9999))
+
+	#Aceptamos conexciones entrantes con el metodo listen , y ademas aplicamos como parametro 
+	#el numero de conexiones que podemos aceptar
+	socket_servidor.listen(2)
+	
+	salida = False
+
+	print ("esperando cliente ...")
+	#Instanciamos un objeto sc (socket cliente) para recibir datos , al recibir datos este 
+	#devolvera tambien un obejto que representa la tupla:IP , puerto
+	socket_cliente , addr = socket_servidor.accept()
+	print ("un cliente se ha conectado ... ")
+
+	while not salida:
+		#Recibimos el mensaje , con el metodo recv recibimos datos y como parametro la cantidad de bytes a escribir
+		recibido = socket_cliente.recv(1024)
+
+		#Si el mensaje recibido es la palabra close se cierra la aplicacion
+		if recibido == "close":
+			break
+
+		#si se reciben los datos nos muestra la IP y el mensaje recibido
+		print str(addr[0]) + " dice: " , recibido
+
+		#Devolvemos el mensaje al cliente
+		socket_cliente.send("mensaje recibido")
+	print "Adios"
+
+	socket_cliente.close()
+	socket_servidor.close()
+
+if __name__ == '__main__':
+	inicio() """
+ 
